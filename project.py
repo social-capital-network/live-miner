66a0629f53757c5de984cc67a58da31cd33deaab
stefian <v_ianta@yahoo.com>
2019-07-09 10:12:32 -0400

    added class Sprint stub to project.py

--- project.py
+++ project.py
@@ -1,10 +1,25 @@
 from time import time
 
+# Class Project is similar to the Block class considering transactions equivalent to contributions (including non monetary) and proof equivalent to Test Proofs
+# ToDo: Add Project initialization, Owner, PM, Repo, Charter, Governance etc
+
 
 class Project:
-    def __init__(self, index, parent_hash, contributions, test_proof, time=time()):
+    def __init__(self, index, previous_hash, transactions, proof, time=time()):
         self.index = index
-        self.parent_hash = parent_hash
+        self.previous_hash = previous_hash
         self.timestamp = time
-        self.contributions = contributions
-        self.test_proof = test_proof
+        self.transactions = transactions
+        self.proof = proof
+
+# Class Sprint is similar to the Block class considering transactions equivalent to contributions (including non monetary) and proof equivalent to Test Proofs
+# ToDo: Add Sprint Duration, and Agile Project initialization, Product Owner, SM, Repo, Charter, Governance etc
+
+
+class Sprint:
+    def __init__(self, index, previous_hash, transactions, proof, time=time()):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = time
+        self.transactions = transactions
+        self.proof = proof
