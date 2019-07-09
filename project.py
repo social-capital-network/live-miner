a3cb79cdf8c3eba623f792f3e68e30cfc04eddc0
stefian <v_ianta@yahoo.com>
2019-07-09 13:22:22 -0400

    added __repr__ functions for classes Project and Sprint

--- project.py
+++ project.py
@@ -15,6 +15,9 @@ class Project:
 # Class Sprint is similar to the Block class considering transactions equivalent to contributions (including non monetary) and proof equivalent to Test Proofs
 # ToDo: Add Sprint Duration, and Agile Project initialization, Product Owner, SM, Repo, Charter, Governance etc
 
+    def __repr__(self):
+        return 'Milestones Index: {}, Previous Hash: {}, QA Proof: {}, Contributions: {}'.format(self.index, self.previous_hash, self.proof, self.transactions)
+
 
 class Sprint:
     def __init__(self, index, previous_hash, transactions, proof, time=time()):
@@ -23,3 +26,6 @@ class Sprint:
         self.timestamp = time
         self.transactions = transactions
         self.proof = proof
+
+    def __repr__(self):
+        return 'Milestones Index: {}, Previous Hash: {}, QA Proof: {}, Contributions: {}'.format(self.index, self.previous_hash, self.proof, self.transactions)
