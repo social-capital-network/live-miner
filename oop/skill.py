1402022c30d868c6e18684b258121f15bbcfe79b
stefian <v_ianta@yahoo.com>
2019-07-12 17:27:00 -0400

    added constructor for the class Skill

--- oop/skill.py
+++ oop/skill.py
@@ -1,10 +1,15 @@
 class Skill:
-    top_speed = 100
+    top_record_speed = 100
     changes = []
     used_at = ['Visa']
-    warnings = []
+    last_used = ''
+    species_url = 'Github'
+    chromozome_api = 'programming'
 
-    def __init__():
+    def __init__(self, starting_top_speed=100):
+        self.protocol = 'W3AI'
+        self.top_speed = starting_top_speed
+        self.warnings = []
 
     def run(self):
         print('I am operating not faster than {}'.format(self.top_speed))
@@ -15,14 +20,17 @@ skill1.run()
 
 # Skill.top_speed = 200
 skill1.used_at.append('Marry Via Programming')
+skill1.warnings.append('New warning')
 
-skill2 = Skill()
+skill2 = Skill(200)
 skill2.run()
 skill2.used_at.append('World Elections 2020')
 print(skill2.used_at)
+print(skill2.warnings)
 
 
-skill3 = Skill()
+skill3 = Skill(250)
 skill3.run()
 skill3.used_at.append('World Wide Skills Genom')
 print(skill3.used_at)
+print(skill3.warnings)
