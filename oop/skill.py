131ac2cc313eebd2aff7dc16746234dc68474e69
stefian <v_ianta@yahoo.com>
2019-07-13 08:00:47 -0400

    added private private __warnings with setter and getter

--- oop/skill.py
+++ oop/skill.py
@@ -9,13 +9,20 @@ class Skill:
     def __init__(self, starting_top_speed=100):
         self.protocol = 'W3AI'
         self.top_speed = starting_top_speed
-        self.warnings = []
+        self.__warnings = []
         self.source = 'github'
         self.exec = 'npm'
 
     def __repr__(self):
         print('Printing...')
-        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.warnings))
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.__warnings))
+
+    def add_warning(self, warning_text):
+        if len(warning_text) > 0:
+            self.__warnings.append(warning_text)
+
+    def get_warnings(self):
+        return self.__warnings
 
     def run(self):
         print('I am operating not faster than {}'.format(self.top_speed))
@@ -28,18 +35,19 @@ skill1.run()
 
 # Skill.top_speed = 200
 skill1.used_at.append('Marry Via Programming')
-skill1.warnings.append('New warning')
+skill1.add_warning('New warning')
+# skill1.__warnings.append([])
 print(skill1)
 
 skill2 = Skill(200)
 skill2.run()
 skill2.used_at.append('World Elections 2020')
 print(skill2.used_at)
-print(skill2.warnings)
+print(skill2.get_warnings())
 
 
 skill3 = Skill(250)
 skill3.run()
 skill3.used_at.append('World Wide Skills Genom')
 print(skill3.used_at)
-print(skill3.warnings)
+print(skill3.get_warnings())
