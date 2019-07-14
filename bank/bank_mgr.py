c644a26a195df722279e782d9361ee5bda0ae3c0
stefian <v_ianta@yahoo.com>
2019-07-14 16:53:11 -0400

    added __init__ to BankMgr class

--- bank/bank_mgr.py
+++ bank/bank_mgr.py
@@ -1,11 +1,13 @@
 class BankMgr:
 
-    def __init__(self, id, interests, accounts, currencies, balance):
-        self.id = id
-        self.interests = interests
-        self.accounts = accounts
-        self.currencies = currencies
+    def __init__(self, balance):
+        self.id = 1
+        self.interests = ['ai', 'blockchain']
+        self.accounts = {'CAD': 1000.0, 'USD': 2000.0, 'python': 314.0}
+        self.currencies = ['CAD', 'USD', 'PYTHON']
         self.balance = balance
 
 
+banker = BankMgr(3314.0)
+print(type(banker))
 print(type(BankMgr))
